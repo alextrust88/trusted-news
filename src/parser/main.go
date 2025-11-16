5ef8e66fce3481ebd747e5677719300ce98a3016
Aleksandr Pavlov <alextrust@Aleksandrs-Mac-mini.local>
2025-11-16 01:20:55 +0100

    fix cicd

--- src/parser/main.go
+++ src/parser/main.go
@@ -7,13 +7,10 @@ import (
 
 func main() {
 	log.Println("News Parser starting...")
-	
 	// TODO: Реализовать парсинг новостей
 	// - RSS ленты
 	// - Веб-сайты
 	// - API источников
-	
 	log.Println("News Parser stopped")
 	os.Exit(0)
 }
-
--- src/parser/main_test.go
+++ src/parser/main_test.go
@@ -6,4 +6,3 @@ func TestMain(t *testing.T) {
 	// TODO: Добавить тесты для парсера
 	t.Log("Parser tests will be added here")
 }
-
